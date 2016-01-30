a30f2d68b4b4abc55c62d0438495903a393498af
Tarnquil <Tarnquil@gmail.com>
2016-01-30 12:26:09 +0000

    Cleaning Up

---
+++ Assets/Scripts/ServerStuff.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ServerStuff : MonoBehaviour
+{
+	
+	public string connectionIP = "127.0.0.1";
+	public int connectionPort = 25001;
+	public NetworkManager test;
+	NetworkDiscovery disc;
+
+	void Start ()
+	{
+		disc = this.gameObject.GetComponent<NetworkDiscovery> ();
+	}
+
+	public void Connect ()
+	{
+		disc.StartAsClient ();
+	}
+
+	public void StartServer ()
+	{
+		disc.StartAsServer ();
+	}
+
+	void OnMatchCreate ()
+	{
+		Debug.Log ("Match Made");
+	}
+}
