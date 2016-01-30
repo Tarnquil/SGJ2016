86628bfadbf42978d721b5cd16e5f6f0ceddf0a8
Tarnquil <Tarnquil@gmail.com>
2016-01-30 18:20:12 +0000

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -62,7 +62,7 @@ public class PlayerController : MonoBehaviour
 		currentSpell.Clear ();
 	}
 
-	public void AddNodeToSpell (string _nodeNumber)
+	public void AddNodeToSpell (int _nodeNumber)
 	{
 		Debug.Log ("FRIED");
 		if (!currentSpell.Contains (_nodeNumber)) {
