f6a66b2a6fcabbbcf15f1b43c6c609e0fba11eda
niknar266 <Kyle@Kyles-MacBook-Pro.local>
2016-01-30 20:20:52 +0000

    kyle stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,17 +11,41 @@ public class PlayerController : MonoBehaviour
 	Text testLabel;
 	[SerializeField]
 	TextAsset spellXmlFile;
-
-	XmlDocument xmlDoc;
-	XmlNodeList spellList;
-	List<int> currentSpell = new List<int> ();
-	int x = 0;
-
-
 	[SerializeField]
 	int health = 100;
 	[SerializeField]
 	int mana = 100;
+
+	public int Health 
+	{
+		get 
+		{
+			return this.health;
+		}
+		set 
+		{
+			UpdateHealth (value);
+		}
+	}
+
+	public int Mana 
+	{
+		get 
+		{
+			return this.mana;
+		}
+		set 
+		{
+			UpdateMana (value);
+		}
+	}
+
+	XmlDocument xmlDoc;
+	XmlNodeList spellList;
+	List<int> currentSpell = new List<int> ();
+	public List<GameObject> spellPrefabs = new List<GameObject>();
+
+
 
 	public MyNetManager test;
 
@@ -39,26 +63,15 @@ public class PlayerController : MonoBehaviour
 		{
 			CheckIfValidSpell(testLabel.text);
 		}
+
+		if(Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			CheckIfValidSpell("1234");
+		}
 	}
 
 
-	public int Health {
-		get {
-			return this.health;
-		}
-		set {
-			UpdateHealth (value);
-		}
-	}
 
-	public int Mana {
-		get {
-			return this.mana;
-		}
-		set {
-			UpdateMana (value);
-		}
-	}
 
 	void ClearSpell ()
 	{
@@ -79,6 +92,21 @@ public class PlayerController : MonoBehaviour
 	{
 		//Check against XML Spells
 
+		if (spellcode == "1234") 
+		{
+			foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")) 
+			{
+
+				if (player.GetComponent<NetworkIdentity> ().isClient && Network.isClient) 
+				{
+					(player.GetComponent<NetPlayerTest>()).CmdScream(spellcode);
+				}
+				else if(player.GetComponent<NetworkIdentity> ().isServer && player.GetComponent<NetworkIdentity> ().isClient)
+				{
+					(player.GetComponent<NetPlayerTest>()).RpcScream(spellcode);
+				}
+			}
+		}
 	}
 
 	void UpdateHealth (int newHealth)
