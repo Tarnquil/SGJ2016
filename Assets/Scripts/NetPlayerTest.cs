c27ed649c950882c03c6024aa2482ef1588bc283
Tarnquil <Tarnquil@gmail.com>
2016-01-30 16:36:09 +0000

    Getting annoyed

---
+++ Assets/Scripts/NetPlayerTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetPlayerTest : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Testing ()
+	{
+		foreach (GameObject cubes in GameObject.FindGameObjectsWithTag("Player")) {
+			Debug.Log ("Testing");
+			cubes.GetComponent<NetPlayerTest> ().Scream ();
+		}
+	}
+
+	public void Scream ()
+	{
+		Debug.Log ("Kyle is a dick");
+	}
+}
